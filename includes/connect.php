d78b54399abdb69eb11246c01b98ed689a4d8c08
Rishabh Goel <rishgoel08@gmail.com>
2024-10-28 03:58:45 -0400

    changes for db username aassword

--- includes/connect.php
+++ includes/connect.php
@@ -1,11 +1,6 @@
-<?php 
-// $con=mysqli_connect('localhost','root','','ecommerce_1');
-$con = new mysqli('localhost','root','','ecommerce_1');
+<?php
+$con = new mysqli('${DBInstanceEndpoint}','admin','${DBPassword}','${DBName}');
 if(!$con){
     die(mysqli_error($con));
 }
-
-
-
-
 ?>
