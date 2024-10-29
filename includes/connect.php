a99566c58911babf0767e20f378536e83a3e4037
Rishabh Goel <rishgoel08@gmail.com>
2024-10-29 10:34:55 -0400

    changes in connect.php

--- includes/connect.php
+++ includes/connect.php
@@ -11,8 +11,6 @@ $con = new mysqli($host, $user, $password, $dbname);
 // Check connection and provide detailed error reporting
 if ($con->connect_error) {
     die("Connection failed: " . $con->connect_error . "<br>Error Number: " . $con->connect_errno);
-} else {
-    echo "Successfully connected to the database: " . $dbname . " at " . $host;
 }
 
 // Further operations (queries) can be placed here
